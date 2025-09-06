4ae633d9d34560de8cab024c08db047937bd892d
simran-bakshi <simsbakshi9@gmail.com>
2025-09-06 15:45:16 +0530

    Fix Vercel deployment: in-memory DB and template paths

--- api/index.py
+++ api/index.py
@@ -1,89 +1,62 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime,timezone,timedelta
+from datetime import datetime, timezone, timedelta
+import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///mydatabase.db"
+# Fix template folder path for Vercel
+app = Flask(
+    __name__,
+    template_folder=os.path.join(os.path.dirname(__file__), '../templates')
+)
+
+# In-memory database (resets on each request, fine for demo)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///:memory:"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 TIMEZONE = timezone(timedelta(hours=0))
+
+# Todo model
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(TIMEZONE), nullable=False)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f"{self.sno} - {self.title}"
 
-@app.route('/', methods=['GET','POST'])
-def hello_world():
-        if request.method=='POST':
-            title = request.form['title']
-            desc = request.form['desc']
-            print(title)
-            print(desc)
-            print("post")
-            todo = Todo(title=title, desc=desc)
-            db.session.add(todo)
-            db.session.commit()
-        alltodo= Todo.query.all()
-        return render_template('index.html',alltodo= alltodo)
+# Create tables on each function invocation
+with app.app_context():
+    db.create_all()
 
-@app.route('/show')
-def products():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return 'this is products page'
-
-
-
-@app.route('/update/<int:sno>', methods=['GET', 'POST'])
-def update(sno):
-    if request.method=="POST":
+# Home route
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo.query.filter_by(sno=sno).first()
-        todo.title = title
-        todo.desc = desc
-        
-        
+        todo = Todo(title=title, desc=desc)
+        db.session.add(todo)
         db.session.commit()
-        return redirect('/') 
+    alltodo = Todo.query.all()
+    return render_template('index.html', alltodo=alltodo)
+
+# Update route
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
+def update(sno):
     todo = Todo.query.filter_by(sno=sno).first()
-    
-    
-    # if request.method == 'POST':
-    #     todo.title = request.form['title']
-    #     todo.desc = request.form['desc']
-    #     db.session.commit()
-    #     return redirect('/')
-    
+    if request.method == "POST":
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        db.session.commit()
+        return redirect('/')
     return render_template('update.html', todo=todo)
 
-
+# Delete route
 @app.route('/delete/<int:sno>')
 def delete(sno):
-   todo = Todo.query.filter_by(sno=sno).first()
-   db.session.delete(todo)
-   db.session.commit()
-   return redirect("/")
-
-
-
-
-
-
-     
-
-
-
-
-
-
-
-
-
-
-  
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
